4a8e11da7e4aad2655d5fe91155ce1b9cb06df59
Ram-Amoncar <ramamonkar444@gmail.com>
2024-06-14 19:09:12 +0530

    Added missing files & folders - Added prettier & its config - Changed overall style - Improved navigation structure - Changed favicon - Added temporary logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,66 +1,52 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'String Pack',
-        social: {
-            github: 'https://github.com/full-pack/string-pack'
-        },
-        sidebar: [
-
-            {
-                label: 'Getting Started',
-                items: [{
-                        label: 'Guide',
-                        items: [
-                            { label: 'install', link: '/pages/install' },
-
-                        ],
-                    },
-
-                    {
-                        label: 'Api',
-                        items: [
-                            { label: 'Padding', link: '/pages/padding' },
-                            { label: 'Merge', link: '/pages/merge' },
-                            { label: 'Compare', link: '/pages/compare/' },
-                            { label: 'Loose Compare', link: '/pages/loosecom' },
-                            { label: 'Capitalize Initial', link: '/pages/capitalizeinitial' },
-                            { label: 'Capitilize word', link: '/pages/capitalizewords' },
-                            { label: 'Case Conversion', link: '/pages/caseconversion' },
-                            { label: 'Case Validation', link: '/pages/casevalidation' },
-                            { label: 'Region Match', link: '/pages/regionmatch' },
-                            { label: 'Loose Region Match', link: '/pages/looseregionmatch' },
-                            { label: 'Is Alpha', link: '/pages/isalpha' },
-                            { label: 'Is Numeric', link: '/pages/isalphanumeric' },
-                        ],
-                    },
-                    {
-                        label: 'Build',
-                        items: [
-                            { label: 'Build', link: '/pages/build' },
-
-                        ],
-                    },
-
-
-                ],
-
-
-
-            }
-        ],
-        title: 'String-pack Doc ',
-        customCss: [
-            // Path to your Tailwind base styles:
-            './src/tailwind.css'
-        ]
-    }), tailwind({
-        // Disable the default base styles:
-        applyBaseStyles: false
-    })]
-});
+    integrations: [
+        starlight({
+            title: 'Full-Pack',
+            favicon: 'favicon.png',
+            logo: {
+                src: '/src/assets/fpLogo.png',
+                replacesTitle: true
+            },
+            sidebar: [
+                {
+                    label: 'String-Pack',
+                    items: [
+                        { label: 'Getting Started', link: '/string-pack/' },
+                        {
+                            label: 'APIs',
+                            items: [
+                                {
+                                    label: 'Functions',
+                                    items: [
+                                        { label: 'merge', link: '/string-pack/merge' },
+                                        { label: 'compare', link: '/string-pack/compare/' },
+                                        { label: 'padding', link: '/string-pack/padding' },
+                                        { label: 'regionMatchers', link: '/string-pack/region-matchers' },
+                                        { label: 'caseConversion', link: '/string-pack/case-conversion' },
+                                        { label: 'caseValidation', link: '/string-pack/case-validation' }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+            social: {
+                github: 'https://github.com/full-pack'
+            },
+            customCss: [
+                // Path to your Tailwind base styles:
+                './src/tailwind.css'
+            ]
+        }),
+        tailwind({
+            // Disable the default base styles:
+            applyBaseStyles: false
+        })
+    ]
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,26 @@
-import starlightPlugin from '@astrojs/starlight-tailwind';
+import starlightPlugin from '@astrojs/starlight-tailwind'
 
 // Generated color palettes
-const accent = { 200: '#b0c9f6', 600: '#1f60e0', 900: '#122f66', 950: '#102245' };
-const gray = { 100: '#f8f6f5', 200: '#f1ecec', 300: '#c6c0c0', 400: '#938988', 500: '#5f5555', 700: '#3f3635', 800: '#2d2424', 900: '#1b1717' };
+const accent = { 200: '#b2cbea', 600: '#1a6bc1', 900: '#133358', 950: '#11253c' }
+const gray = {
+    100: '#f6f6f6',
+    200: '#eeeeee',
+    300: '#c2c2c2',
+    400: '#8b8b8b',
+    500: '#585858',
+    700: '#383838',
+    800: '#272727',
+    900: '#181818'
+}
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    content: ['./src/**/*.{astro, html, js, jsx, md, mdx, svelte, ts, tsx, vue}'],
     theme: {
         extend: {
-            colors: { accent, gray },
-        },
+            colors: { accent, gray }
+        }
     },
-    plugins: [starlightPlugin()],
-};
+    plugins: [starlightPlugin()]
+}
+import starlightPlugin from '@astrojs/starlight-tailwind'
